b7bea9c2de8237af49a2c8318e6d2d351716d061
Kevin Croke <k.croke@shipworks.com>
2022-02-24 14:37:15 -0600

    Make it faster

--- SimpleStrategy.cs
+++ SimpleStrategy.cs
@@ -20,47 +20,50 @@ namespace polyglots.practice._2022
 
         public string Solve()
         {
-            var nextProject = NextProject();
-            List<Project> projectPlan = new List<Project>();
+            var (nextProject, nextContributors) = NextProject();
+            var projectPlan = new List<(Project, List<Contributor>)>();
             while (nextProject != null)
             {
-                projectPlan.Add(nextProject);
+                projectPlan.Add((nextProject, nextContributors));
                 currentDate += nextProject.duration;
-                nextProject = NextProject();
+                (nextProject, nextContributors) = NextProject();
             }
 
             StringBuilder sb = new StringBuilder();
             sb.AppendLine(projectPlan.Count().ToString());
-            foreach (var thisProject in projectPlan)
+            foreach (var (thisProject, thisTeam) in projectPlan)
             {
                 sb.AppendLine(thisProject.name);
-                var team = thisProject.teamComplete(contributors);
-                sb.AppendLine(string.Join(' ', team.Select(p => p.name)));
+                sb.AppendLine(string.Join(' ', thisTeam.Select(p => p.name)));
             }
             return sb.ToString();
         }
 
-        private Project NextProject()
+        private (Project, List<Contributor>) NextProject()
         {
             Project nextProject = null;
+            List<Contributor> nextContributors = null;
             foreach (Project project in projects)
             {
-                if (project.canComplete(contributors) && project.canCompleteBestBefore(currentDate))
+                var projectContributors = project.teamComplete(contributors);
+                if (projectContributors != null && project.canCompleteBestBefore(currentDate))
                 {
                     if (nextProject == null)
                     {
                         nextProject = project;
+                        nextContributors = projectContributors;
                     }
                     else if (nextProject != null && project.pointsByDay > nextProject.pointsByDay)
                     {
                         nextProject = project;
+                        nextContributors = projectContributors;
                     }
                 }
             }
             if (nextProject != null)
                 projects.Remove(nextProject);
 
-            return nextProject;
+            return (nextProject, nextContributors);
         }
 
 
